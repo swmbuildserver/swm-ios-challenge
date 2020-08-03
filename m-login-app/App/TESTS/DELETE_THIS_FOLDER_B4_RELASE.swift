346a819be29ef5353f2b032e9118ba3f51d30310
Normann Joseph <normann.joseph@gmail.com>
2020-08-03 10:52:55 +0200

    cleanup

--- m-login-app/App/TESTS/DELETE_THIS_FOLDER_B4_RELASE.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DELETE_THIS_FOLDER_B4_RELASE.swift
-//  m-login-app
-//
-//  Created by Normann Joseph on 23.06.20.
-//  Copyright © 2020 SWM Services GmbH. All rights reserved.
-//
-
-import Foundation
